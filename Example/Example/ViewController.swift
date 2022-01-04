3bf80197f3859edfbe1d4a532e0a34a5bbc7ae43
ekmacmini43 <ekmacmini43@ekmacmini43s-Mac-mini.local>
2022-01-04 17:34:39 +0545

    Example project created

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -13,15 +13,6 @@ class ViewController: UIViewController {
     //MARK: VC Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
-    }
-    
-    //MARK: Other functions
-    private func setup() {
-        view.translatesAutoresizingMaskIntoConstraints = false
-        let passwordTextField = PasswordTextField(frame: CGRect(x: .zero, y: .zero, width: .zero, height: .zero))
-        view.addSubview(passwordTextField)
-        
     }
 
 }
